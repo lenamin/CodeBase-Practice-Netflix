878adc1cdf02fa50a3a4a965662b665c2bc04eb5
Lena <99120199+lenamin@users.noreply.github.com>
2022-07-07 14:05:24 +0900

    Initial Commit

---
+++ CodeBase-Practice-Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeBase-Practice-Netflix
+//
+//  Created by Lena on 2022/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
