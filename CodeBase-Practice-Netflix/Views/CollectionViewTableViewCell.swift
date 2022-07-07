b2aad5d40839897f903f7b961c20ee6ee131578d
Lena <99120199+lenamin@users.noreply.github.com>
2022-07-07 15:49:47 +0900

    [ADD] Table View 안에 Collection View 넣기

---
+++ CodeBase-Practice-Netflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  CollectionViewTableViewCell.swift
+//  CodeBase-Practice-Netflix
+//
+//  Created by Lena on 2022/07/07.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+       
+        let layout = UICollectionViewFlowLayout()
+
+        layout.itemSize = CGSize(width: 140, height: 200) // 각 collectionViewCell의 크기를 지정해준 것
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+        contentView.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // collectionView의 frame 설정
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+}
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+}
